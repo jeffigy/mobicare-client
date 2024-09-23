f33a8fcda798d742acbd0763f4fd393e4e7c742d
jeffigy <ejeffydev@gmail.com>
2024-09-23 20:29:55 +0800

    feat: Set up react-query provider and axios instance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "@/store/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body data-theme="night" className={`${inter.className} min-h-screen`}>
-        {children}
+        <Provider> {children}</Provider>
       </body>
     </html>
   );

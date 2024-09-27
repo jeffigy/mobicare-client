7947c795cbbb60fea5dda07c0a764ca35b39b19c
jeffigy <ejeffydev@gmail.com>
2024-09-27 22:56:13 +0800

    feat: Added interceptors for refreshing token

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,13 +1,54 @@
-// import { useStore } from "@/store/store";
+import { useStore } from "@/store/store";
 import axios from "axios";
-// const { token } = useStore();
+
 const axiosInstance = axios.create({
   baseURL: process.env.BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    // Authorization: token ? `Bearer ${token}` : "",
   },
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const { token } = useStore.getState();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const { token, setCredentials, clearCredentials } = useStore.getState();
+
+    if (error.response?.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      if (token) {
+        try {
+          const { data } = await axiosInstance.get("/auth/refresh");
+
+          setCredentials(data.accessToken);
+
+          originalRequest.headers.Authorization = `Bearer ${data.token}`;
+          return axiosInstance(originalRequest);
+        } catch (refreshError) {
+          console.log("Session expired");
+          clearCredentials();
+          return Promise.reject(refreshError);
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;

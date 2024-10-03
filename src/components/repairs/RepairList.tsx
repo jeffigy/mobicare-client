a7da059d995e25a0733b58a1e248f59df4294dae
jeffigy <ejeffydev@gmail.com>
2024-10-03 23:09:47 +0800

    feat: Replaced task cards with an initial feature of datatable

--- src/components/repairs/RepairList.tsx
+++ src/components/repairs/RepairList.tsx
@@ -1,26 +1,36 @@
 "use client";
+
 import { useRepairs } from "@/services/queries";
-import React from "react";
-import Repair from "./Repair";
-import { RepairType } from "@/types/Repair";
-import { AxiosApiResponse } from "@/types/ServerResponse";
+import React, { useMemo } from "react";
+import { useRouter } from "next/navigation";
 
 const RepairList = () => {
+  const router = useRouter();
   const { data, isLoading, isError, error } = useRepairs();
-  if (isLoading) {
-    return <p>loading...</p>;
-  }
-  if (isError) {
-    return <p>{(error as AxiosApiResponse).response?.data.message}</p>;
-  }
+
   return (
-    <>
-      {data
-        ? data.map((repair: RepairType) => (
-            <Repair repair={repair} key={repair.id} />
-          ))
-        : "no data found"}
-    </>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Customer Name</th>
+          <th>Device</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map((task) => (
+          <tr
+            className="hover:bg-base-200"
+            key={task.id}
+            onClick={() => router.push(`/dashboard/tasks/${task.id}`)}
+          >
+            <td>{task.customer.name}</td>
+            <td>{task.device.model}</td>
+            <td>{task.status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 

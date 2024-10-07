5a88760434eee79690ff34e30172e2f6a008d817
jeffigy <ejeffydev@gmail.com>
2024-10-07 20:19:26 +0800

    refactor: Added undefined type to prop

--- src/hooks/useFormattedDate.ts
+++ src/hooks/useFormattedDate.ts
@@ -1,7 +1,7 @@
 "use client";
 import { useMemo } from "react";
 
-const useFormattedDate = (dateString: string) => {
+const useFormattedDate = (dateString: string | undefined) => {
   const formattedDate = useMemo(() => {
     if (!dateString) return "";
     const date = new Date(dateString);

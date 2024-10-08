17df91aeb6e3822af423604ea0a869a7669124d7
jeffigy <ejeffydev@gmail.com>
2024-10-08 23:14:13 +0800

    refactor: prettier fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <main className="h-screen place-items-center bg-base-10 px-6 py-24 sm:py-32 lg:px-8">
+    <main className="bg-base-10 h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold text-primary">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-400 sm:text-5xl">
